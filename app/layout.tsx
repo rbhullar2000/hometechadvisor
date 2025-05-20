2180ed589ad13ba2e06f904c4eae316f592958b8
rbhullar2000 <robsbhullar@gmail.com>
2025-05-20 00:37:26 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,14 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-white text-gray-900">
         <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="flex-grow">{children}</main>
+          {/* Fixed Navbar */}
+          <div className="fixed top-0 left-0 w-full z-50 bg-[#0c0c2c]">
+            <Navbar />
+          </div>
+
+          {/* Push content down so it's not hidden behind navbar */}
+          <main className="flex-grow pt-20">{children}</main>
+
           <Footer />
         </div>
       </body>
