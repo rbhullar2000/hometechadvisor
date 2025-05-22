27acd50fb95bee28abface2b1a618a890297c913
rbhullar2000 <robsbhullar@gmail.com>
2025-05-22 14:32:41 -0700

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.hometechadvisor.com', // 🔁 Replace with your actual production domain
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+};
