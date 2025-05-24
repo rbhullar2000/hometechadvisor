5c2d9a11c67b1ec24b629b7e61a20004a6838de1
rbhullar2000 <robsbhullar@gmail.com>
2025-05-23 21:46:46 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,8 +22,8 @@ export default function Navbar() {
           <Image
             src="/images/hometech.png"
             alt="HomeTechAdvisor Logo"
-            width={64}
-            height={64}
+            width={80}
+            height={80}
             className="object-contain"
           />
           <span className="text-lg font-bold whitespace-nowrap">HomeTechAdvisor</span>
