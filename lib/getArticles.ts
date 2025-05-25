def1e894689bb05fc9aa49292619565114f96ac9
rbhullar2000 <robsbhullar@gmail.com>
2025-05-25 06:49:13 -0700

    Update getArticles.ts

--- lib/getArticles.ts
+++ lib/getArticles.ts
@@ -3,11 +3,17 @@ import path from 'path';
 import matter from 'gray-matter';
 
 export function getArticle(slug: string) {
-  const filePath = path.join(process.cwd(), 'content/articles', `${slug}.md`);
+  const basePath = path.join(process.cwd(), 'content/articles');
+  const mdxPath = path.join(basePath, `${slug}.mdx`);
+  const mdPath = path.join(basePath, `${slug}.md`);
 
-  if (!fs.existsSync(filePath)) {
-    return null;
-  }
+  const filePath = fs.existsSync(mdxPath)
+    ? mdxPath
+    : fs.existsSync(mdPath)
+    ? mdPath
+    : null;
+
+  if (!filePath) return null;
 
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const { content, data } = matter(fileContent);
@@ -21,7 +27,9 @@ export function getArticle(slug: string) {
 export function getAllArticleSlugs() {
   const files = fs.readdirSync(path.join(process.cwd(), 'content/articles'));
 
-  return files.map((filename) => ({
-    slug: filename.replace('.md', ''),
-  }));
+  return files
+    .filter((filename) => filename.endsWith('.md') || filename.endsWith('.mdx'))
+    .map((filename) => ({
+      slug: filename.replace(/\.mdx?$/, ''),
+    }));
 }
